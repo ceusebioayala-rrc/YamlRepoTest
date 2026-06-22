7229b8e6ad1f87ef11fc7d8f23a2d1216b1e04bb
Carlos Eusebio <ceusebioayala@academic.rrc.ca>
2026-06-22 18:07:29 -0500

    Adding github actions.

--- goodhello.py
+++ goodhello.py
@@ -12,6 +12,10 @@ def greet():
     """Prints happy weekend"""
     print("Happy Weekend")
 
+def happy():
+    """Prints a happy phrase."""
+    print("I'm feeling happy today!")
+
 if __name__ == "__main__":
     hello()
     bye()
